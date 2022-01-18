563488eb9d56bdc0b30fb3dafe8b9f44589b380f
anlythree <anlythree@anlythreedeMacBook-Pro.local>
2022-01-18 17:25:47 +0800

    fix

--- src/main/java/top/anlythree/demo/controller/DemoController.java
+++ src/main/java/top/anlythree/demo/controller/DemoController.java
@@ -1,9 +1,7 @@
 package top.anlythree.demo.controller;
 
 import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiOperation;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,8 +15,8 @@ public class DemoController {
 
     @GetMapping("/getDemoStr")
     @ApiOperation("getDemoStr")
-    public String getDemoStr(){
-        String demoStr = "demoStr:controller链接成功。当前时间："+ LocalDateTime.now();
+    public String getDemoStr() {
+        String demoStr = "demoStr:controller链接成功。当前时间：" + LocalDateTime.now();
         System.out.println(demoStr);
         return demoStr;
     }
