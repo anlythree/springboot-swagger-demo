89eef78787293d193db77cfca7c7ea1b35dd1229
anlythree <15610462126@163.com>
2022-06-13 16:46:05 +0800

    sh

--- build/anlyrestart.sh
+++ build/anlyrestart.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 ##设置报错就退出
 set -e
-##当前电脑上的jar包目录！！！该目录会被先删除后更新，设置前务必确认，脚本不会做备份！！！
-CURRENT_PATH="/"
+echo "该脚本的处理内容如下，执行前请务必确认是否需要这些步骤："
+echo "本地处理（1、删除原maven打包目录（变量CURRENT_JAR_PATH代表的文件夹），2、运行maven命令重新打包，3、解压tar包来生成新jar包，4、复制新jar包至服务器并重命名后带当前时间）"
+echo "连接服务器（5、把旧jar包进程杀死，6、把旧jar包名字后加上时间，7、重命名新jar包并启动）"
+
+
+
+##当前电脑上的jar包目录！！！该目录会被先删除后更新，设置前务必确认，脚本不会做备份！！！（/结尾）
+CURRENT_PATH="/Users/anlythree/my/springboot-swagger-demo/"
 # shellcheck disable=SC1068
-##服务器的jar包位置
-SERVER_JAR_PATH="/home/jar"
+##服务器的jar包位置(/结尾)
+SERVER_JAR_PATH="/home/jar/"
 ##jar包名称
 JAR_NAME="springboot-swagger-demo-1.0-SNAPSHOT-boot.jar"
-
 
 ##当前时间
 CURRENT_TIME=$(date "+%Y-%m-%d %H:%M:%S")
@@ -20,54 +25,66 @@ echo "startOf本地处理（1、删除原maven打包目录，2、运行maven命�
 if [ ! -e $CURRENT_JAR_PATH ]
   then
     echo "${CURRENT_JAR_PATH}目录不存在"
-    return
+    exit 1
 fi
 if [ $CURRENT_PATH = "/" ]
   then
     echo "CURRENT_PATH为根目录！该shell会先删除CURRENT_PATH后再重新创建！所以先修改CURRENT_PATH后再运行该脚本！"
-    return
+    exit 1
 fi
 
 ##删除旧jar包位置，命令很危险！！前边有两个关于路径的校验
-#rm -rf ${CURRENT_JAR_PATH}
+rm -rf ${CURRENT_JAR_PATH}
 
 ##重新打jar包
 cd ${CURRENT_PATH}
 mvn clean package -Dmaven.test.skip=true
+##复制jar包至服务器
+
+/usr/bin/expect <<EOF
+  ##scp复制时把文件名修改为带日期格式的
+  spawn scp -r "${CURRENT_JAR_PATH}${CURRENT_JAR_PATH}" anlyconnect@43.132.237.240:${SERVER_JAR_PATH}${CURRENT_JAR_PATH} -P 233
+  ##输入密码
+  expect "*password"
+  send "Anly233\r"
+  ##scp复制完成后退出服务器
+  send "exit"
+EOF
+
 echo "endOf本地处理（1、删除原maven打包目录，2、运行maven命令重新打包，3、解压tar包来生成新jar包，4、复制新jar包至服务器并重命名后带当前时间）"
 ##endOf本地处理（1、删除原maven打包目录，2、运行maven命令重新打包，3、解压tar包来生成新jar包，4、复制新jar包至服务器并重命名后带当前时间）
 
-##startOf连接服务器（1、把旧jar包进程杀死，2、把旧jar包名字后加上时间，3、重命名新jar包并启动）
-echo "startOf连接服务器（1、把旧jar包进程杀死，2、把旧jar包名字后加上时间，3、重命名新jar包并启动）"
+##startOf连接服务器（5、把旧jar包进程杀死，6、把旧jar包名字后加上时间，7、重命名新jar包并启动）
+echo "startOf连接服务器（5、把旧jar包进程杀死，5、把旧jar包名字后加上时间，7、重命名新jar包并启动）"
 /usr/bin/expect <<EOF
 
-##连接到远程服务器
-spawn ssh anlyconnect@43.132.237.240 -p 233
+  ##连接到远程服务器
+  spawn ssh anlyconnect@43.132.237.240 -p 233
 
-##捕捉密码并登录
-expect "*password"
-send "Anly233\r"
+  ##捕捉密码并登录
+  expect "*password"
+  send "Anly233\r"
 
-expect "*Lastlogin:*"
+  expect "*Lastlogin:*"
 
-##查看已经在运行的jar包，并kill掉
-#send "result = ps -ef|grep bus-arrive\r"
+  ##查看已经在运行的jar包，并kill掉
+  #send "result = ps -ef|grep bus-arrive\r"
 
-#echo "result:${result}"
+  #echo "result:${result}"
 
-#send "OLD_PID = ps -f | grep java | grep ${JAR_NAME} |awk '{print \$2}'"
-#
-#send "kill -9 ${OLD_PID}\r"
+  #send "OLD_PID = ps -f | grep java | grep ${JAR_NAME} |awk '{print \$2}'"
+  #
+  #send "kill -9 ${OLD_PID}\r"
 
-## expect ""
-#echo "杀掉旧jar包"
+  ## expect ""
+  #echo "杀掉旧jar包"
 
-send "exit\r"
+  send "exit\r"
 
-interact
+  interact
 
 EOF
 
-echo "endOf连接服务器（1、把旧jar包进程杀死，2、把旧jar包名字后加上时间，3、重命名新jar包并启动）"
-##endOf连接服务器（1、把旧jar包进程杀死，2、把旧jar包名字后加上时间，3、重命名新jar包并启动）
+echo "endOf连接服务器（5、把旧jar包进程杀死，6、把旧jar包名字后加上时间，7、重命名新jar包并启动）"
+##endOf连接服务器（5、把旧jar包进程杀死，6、把旧jar包名字后加上时间，7、重命名新jar包并启动）
 
